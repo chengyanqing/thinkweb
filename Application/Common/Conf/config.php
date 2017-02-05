ebac67abc634811815d2d1477ca4c7aab07352b6
陈燕青 <m18118819935@163.com>
2017-02-05 17:56:44 +0800

    配置文件

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,14 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
+    'URL_MODEL'=>2,  // 隐藏index.php文件
+    'DEFAULT_MODULE'        =>  'Home',  // 默认模块
+    // 配置系统静态资源
+    "SITE_URL"=>"http://" . $_SERVER["HTTP_HOST"],  // 配置当前域名
+
+    'TMPL_PARSE_STRING' => [
+        // 后台管理
+        'ADMIN_CSS_URL' => __ROOT__ . '/Public/Admin/css' ,
+        'ADMIN_JS_URL' => __ROOT__ . '/Public/Admin/js' ,
+        'ADMIN_IMG_URL' => __ROOT__ . '/Public/Admin/image' ,
+    ] ,
 );
