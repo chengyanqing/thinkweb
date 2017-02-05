d88c4b852f1469a14a419fb559e155fdaa45c7df
陈燕青 <m18118819935@163.com>
2017-02-05 13:10:38 +0800

    登录页面控制器编写

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -1,8 +1,18 @@
 <?php
 namespace Admin\Controller;
 use Think\Controller;
+/**
+ * 创建人:潘朝田
+ * 创建时间:17-2-5 下午12:03
+ * 创建功能:后台管理系统首页控制器
+ */
 class IndexController extends Controller {
+    /**
+     * 创建人:潘朝田
+     * 创建时间:17-2-5 下午12:03
+     * 创建功能:后台管理系统首页控制器
+     */
     public function index(){
-        
+        $this->redirect('Public/login') ;
     }
 }
---
+++ Application/Admin/Controller/PublicController.class.php
@@ -0,0 +1,17 @@
+<?php
+namespace Admin\Controller;
+/**
+ * 创建人:潘朝田
+ * 创建时间:2017-2-5 12:05:36
+ * 创建功能:登录控制器
+ */
+class PublicController {
+    /**
+     * 创建人:潘朝田
+     * 创建时间:2017-2-5 12:05:36
+     * 创建功能:登录方法
+     */
+    public function login(){
+        echo  '登录方法...' ;
+    }
+}
